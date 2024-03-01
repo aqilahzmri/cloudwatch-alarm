9539639b19850dbe2a230af9297a841105cc0a89
aqilahzmri <127908263+aqilahzmri@users.noreply.github.com>
2024-03-01 13:00:52 +0800

    add dev db alarm

--- main.tf
+++ main.tf
@@ -246,6 +246,118 @@ resource "aws_cloudwatch_metric_alarm" "swap_usage_too_high" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "devdb_cpu_alarm" {
+  alarm_name          = "devdb_cpu_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = 900 # 15 minutes
+  statistic           = "Average"
+  threshold           = min(max(80, 0), 100)
+  alarm_description   = "Alarm when CPU exceeds 80% over last 15 minutes"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "burst_balance_too_low" {
+  alarm_name          = "devdb_burst_balance"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "BurstBalance"
+  namespace           = "AWS/RDS"
+  period              = 600 # 15 minutes
+  statistic           = "Average"
+  threshold           = min(max(20, 0), 100)
+  alarm_description   = "Average database storage burst balance over last 10 minutes too low, expect a significant performance drop soon"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_credit_balance_too_low" {
+  alarm_name          = "devdb_cpu_credit_balance_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUCreditBalance"
+  namespace           = "AWS/RDS"
+  period              = 600
+  statistic           = "Average"
+  threshold           = max(20, 0)
+  alarm_description   = "Average database CPU credit balance over last 10 minutes too low, expect a significant performance drop soon"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
+  alarm_name          = "devdb_queue_depth_too_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "DiskQueueDepth"
+  namespace           = "AWS/RDS"
+  period              = 600
+  statistic           = "Average"
+  threshold           = max(64, 0)
+  alarm_description   = "Average database disk queue depth over last 10 minutes too high, performance may suffer"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "freeable_memory_too_low" {
+  alarm_name          = "devdb_freeable_memory_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  period              = 600
+  statistic           = "Average"
+  threshold           = max(64000000, 0) # 64 Megabyte in Byte
+  alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
+  alarm_name          = "devdb_free_storage_space_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = 600
+  statistic           = "Average"
+  threshold           = max(2000000000, 0) # 2 Gigabyte in Byte
+  alarm_description   = "Average database free storage space over last 10 minutes too low"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "swap_usage_too_high" {
+  alarm_name          = "devdb_swap_usage_too_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "SwapUsage"
+  namespace           = "AWS/RDS"
+  period              = 600
+  statistic           = "Average"
+  threshold           = max(256000000, 0) # 256 Megabyte in Byte 
+  alarm_description   = "Average database swap usage over last 10 minutes too high, performance may suffer"
+  alarm_actions       = [aws_sns_topic.alarm_email.arn]
+  dimensions = {
+    DBInstanceIdentifier = "devdb"
+  }
+}
+
 #terraform backend configuration, save into the s3 bucket
 terraform {
   backend "s3" {
