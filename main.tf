52c30838154fa302f7155022552453e7ccc785a3
Aqilah <aqilah.maisarah@regovtech.com>
2024-03-01 15:03:55 +0800

    update

--- main.tf
+++ main.tf
@@ -43,8 +43,8 @@ resource "aws_lambda_function" "alarm_email_function" {
   description      = "CloudWatch alarms email formatter"
   timeout          = 60
   role             = aws_iam_role.lambda_role.arn
-  source_code_hash = filebase64sha256("cwalarm-formatted-email-lambda.py")
-  filename        = "cwalarm-formatted-email-lambda.py"
+  source_code_hash = filebase64sha256("lambda_send_email.zip")
+  filename        = "lambda_send_email.zip"
 
   environment {
     variables = {
